66900dbaef70f59a17e06d953d196073b3b19288
Markia Newman <mnewman87@gmail.com>
2019-11-03 14:50:39 -0500

    Initial Module 8 commit

---
+++ site/module-8-solution/app.js
@@ -0,0 +1,78 @@
+(function () {
+  'use strict';
+
+  angular.module('NarrowItDownApp', [])
+  .controller('NarrowItDownController', NarrowItDownController )
+  .service('MenuSearchService', MenuSearchService)
+  .directive('foundItems', FoundItems);
+
+  NarrowItDownController.$inject = ['MenuSearchService'];
+  function NarrowItDownController(MenuSearchService) {
+    var list = this;
+    list.searchTerm = "";
+    list.narrowItDown =function(){
+
+      var promise = MenuSearchService.getMatchedMenuItems(list.searchTerm);
+    promise.then(function (response)
+    {
+      list.items = response;
+    })
+    };
+
+    list.removeItem = function (itemIndex) {
+      MenuSearchService.removeItem(itemIndex);
+    };
+
+  }
+
+  function FoundItems(){
+    var ddo={
+      templateUrl: 'foundItems.html',
+      scope:{
+        items: '<',
+        onRemove: '&'
+      },
+    controller: NarrowItDownDirectiveController,
+    controllerAs: 'list',
+    bindToController: true
+    }; 
+    return ddo;
+  }
+
+
+function NarrowItDownDirectiveController() {
+  var list = this;
+}
+
+
+  MenuSearchService.$inject =['$http']
+  function MenuSearchService($http) {
+
+    var service = this;
+    var items = [];
+
+    service.removeItem = function (itemIndex) {
+      items.splice(itemIndex, 1);
+    };
+
+    service.getMatchedMenuItems = function (searchTerm) {
+      var foundItems = [];
+      //TODO Implement
+      return $http({
+        method:"GET",
+        url: ("https://davids-restaurant.herokuapp.com/menu_items.json")
+      }).then(function(result){
+        for (var i = 0; i < result.data.menu_items.length; i++)
+        {
+          var description = result.data.menu_items[i].description;
+          if(description.toLowerCase().indexOf(searchTerm) !== -1){
+            foundItems.push( result.data.menu_items[i]);
+          }
+        }
+
+        items = foundItems;
+        return items;
+      });
+    };
+  }
+})();
